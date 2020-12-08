5472eabc9ad8efaee7a0bce178c7aa23de6e253d
sunblob <morkovgleb@yandex.ru>
2020-12-08 21:27:44 +0300

    finished toolbar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn
+          to="/camera"
+          flat
+          round
+          icon="eva-camera-outline"
+          size="18px"
+          dense
+          class="large-screen-only q-mr-sm"
+        />
+        <q-separator vertical spaced class="large-screen-only" />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasargram
+        </q-toolbar-title>
+        <q-btn
+          to="/"
+          flat
+          round
+          icon="eva-home-outline"
+          size="18px"
+          dense
+          class="large-screen-only"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab icon="eva-home-outline" to="/" exact />
+        <q-route-tab icon="eva-camera-outline" to="/camera" exact />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.q-footer .q-tab__icon {
+  font-size: 30px;
+}
+
+.q-toolbar {
+  @media (min-width: $breakpoint-sm-min) {
+    height: 77px;
+  }
+}
+
+.q-toolbar__title {
+  font-size: 30px;
+  @media (max-width: $breakpoint-xs-max) {
+    text-align: center;
+  }
+}
+</style>
